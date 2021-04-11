1fb48803faf90e1d6a30549940b3b3746141efcd
SuhasBRao <raos04567@gmail.com>
2021-04-11 21:13:04 +0530

    Without using inbuilt function

--- Reverse_a_string.py
+++ Reverse_a_string.py
@@ -1,5 +1,5 @@
 '''
-A simple python program to reverse a given input array
+A simple python program to reverse a given input array (without using inbuilt function)
 '''
 from sys import stdin,stdout
 
@@ -7,4 +7,12 @@ def get_array():
     return list(map(int, stdin.readline().strip().split()))
 
 arr = get_array()
-print(arr[::-1])
+start = 0
+end = len(arr) - 1
+
+while start < end:
+    arr[start], arr[end] = arr[end],arr[start]
+    start += 1
+    end -= 1 
+
+print(arr)
