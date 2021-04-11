6835a3aca0e871370dea5fe2e20a40b431f74015
SuhasBRao <raos04567@gmail.com>
2021-04-11 18:25:59 +0530

    Complete function

--- Reverse_a_string.py
+++ Reverse_a_string.py
@@ -2,3 +2,9 @@
 A simple python program to reverse a given input array
 '''
 from sys import stdin,stdout
+
+def get_array():
+    return list(map(int, stdin.readline().strip().split()))
+
+arr = get_array()
+print(arr[::-1])
