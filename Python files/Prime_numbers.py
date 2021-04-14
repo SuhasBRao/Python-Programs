a4edd470211b17ef4acf3bb5e63731c30a928bb6
SuhasBRao <raos04567@gmail.com>
2021-04-14 18:09:31 +0530

    Added another fucntion with lesser complexity.

--- Python files/Prime_numbers.py
+++ Python files/Prime_numbers.py
@@ -1,11 +1,13 @@
 '''
-Function to generate prime numbrs less than a given number n
+Function to generate prime numbrs less than a given number n.
 '''
 from sys import stdin,stdout
+from math import sqrt
 N = int(stdin.readline())
 
 def is_it_prime(num):
     # This function checks if a number is prime or not
+    # has O(n) complexity.
     if num < 2:
         return False
     for i in range(2,num):
@@ -13,12 +15,25 @@ def is_it_prime(num):
             return False
     return True
 
+def is_prime(num):
+    # This function checks if a number is prime or not
+    # This has O(sqrt(n)) complexity.
+    if num < 2:
+        return False
+    for item in range(2,int(sqrt(num))):
+        if num % item == 0:
+            return False 
+    return True
+
 def generate_prime(n):
     # the funciton genrates prime numbrs less than n
     # using is_it_prime() function defined above
     for i in range(1,n + 1):
         val = is_it_prime(i)
-        if val == True:
+        val_1 = is_prime(i)
+        if (val == True) and (val_1 == True):
             stdout.writelines(str(i) + ' ')
+            stdout.writelines(str(i) + ' ')
+            print()
 
 generate_prime(N)
