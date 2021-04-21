eef665a671b48e3a2bf2649e07eb6956e3ed2fc8
SuhasBRao <raos04567@gmail.com>
2021-04-22 02:46:39 +0530

    Added merge sort

---
+++ Python files/merge_sort.py
@@ -0,0 +1,46 @@
+'''
+This python program implements merge_sort on an 
+array of some size n.
+'''
+from sys import stdin,stdout
+
+def getInputs():
+    return list(map(int,stdin.readline().strip().split()))
+
+def merge_sort(arr):
+
+    if len(arr) > 1:
+        mid = len(arr)//2
+        lefthalf = arr[:mid]
+        righthalf = arr[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                arr[k] = lefthalf[i]
+                i += 1
+            else:
+                arr[k] = righthalf[j]
+                j += 1
+            k += 1
+
+        while i < len(lefthalf):
+            arr[k] = lefthalf[i]
+            i +=1
+            k += 1
+        while j < len(righthalf):
+            arr[k] = righthalf[j]
+            j += 1
+            k += 1
+
+
+
+stdout.writelines('Enter array inputs:')
+arr = getInputs()
+merge_sort(arr)
+print(arr)
