6d3980ad142d347dd19f171b0a0aaf998d28fcb3
Suhas B Rao <raos04567@gmail.com>
2021-04-25 14:27:54 +0530

    Added light_bulb.py

---
+++ Light_bulb.py
@@ -0,0 +1,69 @@
+'''
+This python program implements the solution for the given problem.
+The problem can be found in the below link:
+https://py.checkio.org/en/mission/lightbulb-intro/
+
+'''
+
+from datetime import datetime
+from typing import List
+
+def sum_light(els: List[datetime]) -> int:
+    """
+        how long the light bulb has been turned on
+    """
+    total_on = 0
+    switch_on = els[::2]
+    switch_off = els[1::2]
+
+    for indx in range(len(switch_on)):
+        diff = switch_off[indx] - switch_on[indx]
+        total_on += diff.total_seconds()
+
+    return int(total_on)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(sum_light([
+        datetime(2015, 1, 12, 10, 0 , 0),
+        datetime(2015, 1, 12, 10, 10 , 10),
+        datetime(2015, 1, 12, 11, 0 , 0),
+        datetime(2015, 1, 12, 11, 10 , 10),
+    ]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0 , 0),
+        datetime(2015, 1, 12, 10, 10 , 10),
+    ]) == 610
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0 , 0),
+        datetime(2015, 1, 12, 10, 10 , 10),
+        datetime(2015, 1, 12, 11, 0 , 0),
+        datetime(2015, 1, 12, 11, 10 , 10),
+    ]) == 1220
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0 , 0),
+        datetime(2015, 1, 12, 10, 10 , 10),
+        datetime(2015, 1, 12, 11, 0 , 0),
+        datetime(2015, 1, 12, 11, 10 , 10),
+        datetime(2015, 1, 12, 11, 10 , 10),
+        datetime(2015, 1, 12, 12, 10 , 10),
+    ]) == 4820
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0 , 0),
+        datetime(2015, 1, 12, 10, 0 , 1),
+    ]) == 1
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0 , 0),
+        datetime(2015, 1, 12, 10, 0 , 10),
+        datetime(2015, 1, 12, 11, 0 , 0),
+        datetime(2015, 1, 13, 11, 0 , 0),
+    ]) == 86410
+
+    #print("The first mission in series is completed? Click 'Check' to earn cool rewards!")
