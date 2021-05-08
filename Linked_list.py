59880e39ec43a78d3f86e132d7df37057529bae3
SuhasBRao <raos04567@gmail.com>
2021-05-08 17:39:46 +0530

    added remove_at function

--- Linked_list.py
+++ Linked_list.py
@@ -22,9 +22,27 @@ class linkedList:
         # This function is used to insert node at the end of the singly linked list
         temp_node = Node(data)
         ptr = self.head
-        while ptr.next != None:
-            ptr = ptr.next
-        ptr.next = temp_node
+        if ptr == None:
+            self.head = temp_node
+        else:
+            while ptr.next != None:
+                ptr = ptr.next
+            ptr.next = temp_node
+
+    def insert_values(self, values : [list]):
+        # inserts multiple values at the same time
+        for item in values:
+            self.insert_at_end(item)
+        
+    def get_len(self):
+        # prints the lenght of the linkedlist
+        count = 0
+        ptr = self.head
+        while ptr:
+            count += 1
+            ptr = ptr.next 
+        return count
+    
 
     def insert_at_pos(self,pos,data):
         # The function is used to insert node at a specified position
@@ -44,7 +62,23 @@ class linkedList:
             temp_node.next = ptr.next
             ptr.next = temp_node
 
-
+    def remove_at(self,index):
+        # removes the node at a particular position
+        if index < 0 or index > self.get_len():
+            print('Invalid index!')
+            return
+        if index == 0:
+            self.head = self.head.next
+            return
+        pos = 0
+        ptr = self.head
+        while ptr:
+            ptr = ptr.next
+            pos += 1
+            if pos == index - 1:
+                ptr.next = ptr.next.next
+                break
+        
     def display_list(self):
         # Funtion is used to display the singly linked list created
         if self.head == None:
@@ -61,11 +95,14 @@ class linkedList:
 # Alter the below segments to create a linked list with required data field
 if __name__ == '__main__':
     mylist = linkedList()   # create an instance of the class
-
+    
+    mylist.insert_at_end(11)
     mylist.insert_at_beg(5) # insert 5 at beginning
     mylist.insert_at_beg(89)  # insert 89 at beginning
-    mylist.insert_at_end(11) # insert 0 at end
-    mylist.display_list()
 
     mylist.insert_at_pos(99, 0)
+    mylist.insert_values(['I','am','sUHAS','B','Rao'])
     mylist.display_list()
+    mylist.remove_at(3)
+    mylist.display_list()
+    mylist.get_len()
