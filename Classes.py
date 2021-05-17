e13f64c2694e516445317af8717cc455da2d15df
SuhasBRao <raos04567@gmail.com>
2021-05-17 22:27:15 +0530

    Added new classes problem

---
+++ Classes.py
@@ -0,0 +1,44 @@
+'''
+This program implements solution for the following hackerrank problem.
+Problem -> https://www.hackerrank.com/challenges/class-2-find-the-torsional-angle/problem
+'''
+import math
+
+class Points(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __sub__(self, no):
+        x_n = self.x - no.x
+        y_n = self.y - no.y
+        z_n = self.z - no.z
+        return Points(x_n, y_n, z_n)
+
+    def dot(self, no):
+        return (self.x*no.x + self.y *no.y + self.z*no.z)
+        
+    def cross(self, no):
+        x_n = self.y*no.z - self.z*no.y
+        y_n = self.z*no.x - self.x*no.z
+        z_n = self.x*no.y - self.y*no.x
+        return Points(x_n, y_n, z_n)
+        
+    def absolute(self):
+        return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
+
+if __name__ == '__main__':
+    points = list()
+    for i in range(4):
+        a = list(map(float, input().split()))
+        points.append(a)
+
+    a, b, c, d = Points(*points[0]), Points(*points[1]), Points(*points[2]), Points(*points[3])
+    x = a.dot(b)
+    y = b.dot(a)
+    x = (b - a).cross(c - b)
+    y = (c - b).cross(d - c)
+    angle = math.acos(x.dot(y) / (x.absolute() * y.absolute()))
+
+    print("%.2f" % math.degrees(angle))
