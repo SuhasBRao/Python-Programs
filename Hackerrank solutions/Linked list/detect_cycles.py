aaf76c1abc2403cf0e6c01abd92fea87b369fc19
SuhasBRao <raos04567@gmail.com>
2021-05-18 23:50:17 +0530

    Added new lll problem solutions

---
+++ Hackerrank solutions/Linked list/detect_cycles.py
@@ -0,0 +1,94 @@
+'''
+Problem statement -> https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
+
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data)
+
+        node = node.next
+
+        if node:
+            print(sep)
+
+# Complete the has_cycle function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def has_cycle(head):
+    ptr = head
+    my_node_set = set()
+    while ptr:
+        if ptr in my_node_set:
+            return 1
+            break
+        else:
+            my_node_set.add(ptr)
+        ptr = ptr.next
+    
+    return 0
+
+
+if __name__ == '__main__':
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        index = int(input())
+
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        extra = SinglyLinkedListNode(-1)
+        temp = llist.head
+
+        for i in range(llist_count):
+            if i == index:
+                extra = temp
+
+            if i != llist_count-1:
+                temp = temp.next
+
+        temp.next = extra
+
+        result = has_cycle(llist.head)
+
+        print(result)
