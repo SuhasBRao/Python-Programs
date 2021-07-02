c7ec8a12c3b8cea01d0f7ea529a914e293cc4128
SuhasBRao <raos04567@gmail.com>
2021-07-02 18:55:47 +0530

    added few comments

--- Python files/Sorting Algorithms/Brute-force/insertion_sort.py
+++ Python files/Sorting Algorithms/Brute-force/insertion_sort.py
@@ -2,7 +2,7 @@
 
 # Check out Insertion sort Description here -> (https://www.geeksforgeeks.org/insertion-sort/)
 def checkFurther(i,key,arr):
-    
+    # recusrsive function that checks for smaller elements
     if i >= 0:
         if key < arr[i-1]:
             return checkFurther(i-1,key,arr)
@@ -18,18 +18,18 @@ def checkFurther(i,key,arr):
     return arr
 
 def insertionSort(arr):
+    
     for i in range(1,len(arr)):
         print()
         print('#####')
+        
         key = arr[i]
         if key < arr[i -1]:
             arr = checkFurther(i-1,key,arr)
-            print(arr)
-                
-                
+            
     
 
 #arr = [4,3,2,10,12,1,5,6]
-arr = [7, 1, 3, 2, 4, 5, 6]
-#arr = [12,1,2,3]
+#arr = [7, 1, 3, 2, 4, 5, 6]
+arr = [2,3,4,1,5]
 insertionSort(arr)
