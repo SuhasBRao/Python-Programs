2ff25f826770a7c3de248b19af5babe853f2f32a
SuhasBRao <raos04567@gmail.com>
2021-07-06 09:32:23 +0530

    added modified arr manipulation and inpt.txt

--- arr_manipulation.py
+++ arr_manipulation.py
@@ -3,31 +3,67 @@ import os
 import random
 import re
 import sys
+import numpy as np
+
+def add(x,k):
+    return x + k
 
 def arrayManipulation(n, queries):
-    
+
     arr = [0]*n
-    for query in queries:
-        a = query[0] - 1
-        b = query[1] - 1
-        k = query[2]
-        for indx in range(a,b+1):
-            new_val = k + arr[indx]
-            arr[indx] = new_val
+    
+    q = iter(queries)
+    Done = False
+    for q in queries:
+        #print(next(q))
+        a, b, k = q
+        indxs = iter(range(a - 1,b))
+        #print(a,b,k)
+        D = False
+        while not D:
+            try:
+                    #print(next(indxs))
+                arr[next(indxs)] += k
+            except:
+                D = True
+            
+            #print('##########')
         
-    return arr
-        
-    # Write your code here
-def getINputs():
-    return list(map(int, sys.stdin.readline().strip().split()))
+    return max(arr)
+
+    '''for q in queries:
+        a,b,k = q
+        x = lambda n: n + k
+
+        #arr[a-1 : b] = map(x,arr[a-1:b])
+
+    print(arr)
+    return max(arr)'''
+
+
+def getINputs(line):
+    return list(map(int, line.split()))
 
 if __name__ == '__main__':
     
-    n,m = getINputs()
+    #n,m = getINputs()
     queries = []
-
+    '''
     for _ in range(m):
         queries.append(list(map(int, input().rstrip().split())))
 
     result = arrayManipulation(n, queries)
     print(result)
+    '''
+    with open('inpt.txt', 'r') as file:
+        input_lines = [line.strip() for line in file]
+    
+    #print(input_lines)
+    n,m = getINputs(input_lines[0])
+    
+    for q in input_lines[1:]:
+        queries.append(getINputs(q))
+
+    print(len(queries))
+
+    print(arrayManipulation(n,queries)) 
