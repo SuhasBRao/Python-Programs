19c53b95536ba5f50e9978c405d65304eadc748c
@SuhasBRao@ <raos04567@gmail.com>
2021-07-17 19:42:53 +0530

    added diagonal_diff.py

---
+++ Hackerrank solutions/Data structures/diagonal_diff.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    pdsum = 0
+    sdsum = 0
+    for i in range(n):
+        pdsum += arr[i][i]
+        sdsum += arr[i][n - i - 1]
+    #print(pdsum, sdsum)
+    return abs(pdsum - sdsum)
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    print(result)
