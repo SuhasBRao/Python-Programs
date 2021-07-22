18027c670f84394ab6e64221f51fc10dd7e85653
@SuhasBRao@ <raos04567@gmail.com>
2021-07-22 20:59:28 +0530

    added solution to bill_division

---
+++ Hackerrank solutions/Data structures/bill_division.py
@@ -0,0 +1,37 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    total_bill = sum([x for i,x in enumerate(bill) if i!= k])
+    b_actual = int(total_bill/2)
+    if b == b_actual:
+        print('Bon Appetit')
+    else:
+        print(b - b_actual)
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
