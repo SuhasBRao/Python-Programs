ccc8b3da2e2eb0bb8781e9a8d43775e406d71086
@SuhasBRao@ <raos04567@gmail.com>
2021-07-24 13:38:00 +0530

    memoised the recursive fibonacci

--- recursive_Fib.py
+++ recursive_Fib.py
@@ -3,19 +3,19 @@ Fibonacci sequence value.
 Your code should have the same input/output as the 
 iterative code in the instructions."""
 
-def get_fib(position):
+def get_fib(position, memo ={}):
+    if position in memo:
+        return memo[position]
     if position == 0:
         return 0
     if position == 1:
         return 1
     else:
-        
-        return get_fib(position-1) + get_fib(position-2)
-        
-    
+        memo[position] = get_fib(position-1,memo) + get_fib(position-2, memo)
+        return memo[position]
     #return -1
 
 # Test cases
 print(get_fib(4))
 print(get_fib(5))
-print(get_fib(6))
+print(get_fib(50))
