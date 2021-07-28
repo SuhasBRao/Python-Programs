a6cc73b5dfdec62605dfc0cb77cdd06f6c3e7478
@SuhasBRao@ <raos04567@gmail.com>
2021-07-28 16:04:55 +0530

    added vvce problem solution

--- VVCE problems/convert to palindrome.py
+++ VVCE problems/convert to palindrome.py
@@ -2,13 +2,17 @@ s = input()
 
 ori = s
 if s == s[::-1]:
-    print('Null')
+    print('NULL')
 else:
     i = 0
     while True:
         i += 1
         if s == s[::-1]:
+            print(ori[i-1::-1])
+            break
+        if i >= len(s):
+            print(ori[i::-1])
             break
         s = s[i+1:]
         
-    print(ori[i-1::-1])
+    
