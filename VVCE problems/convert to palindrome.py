2c5ec365e2d7ce1ce9950356df77f98f0204db6a
SuhasBRao <raos04567@gmail.com>
2021-09-08 22:48:11 +0530

    Updated convert to palindrome

--- VVCE problems/convert to palindrome.py
+++ VVCE problems/convert to palindrome.py
@@ -1,18 +1,29 @@
-s = input()
 
-ori = s
-if s == s[::-1]:
-    print('NULL')
-else:
-    i = 0
-    while True:
-        i += 1
-        if s == s[::-1]:
-            print(ori[i-1::-1])
-            break
-        if i >= len(s):
-            print(ori[i::-1])
-            break
-        s = s[i+1:]
-        
+def convertPalindrome(s):
+    ori = s
+    if s == s[::-1]:
+        #print('NULL')
+        return 0
+    else:
+        i = 0
+        while True:
+            s = ori[i:]
+
+            if i >= len(ori):
+                #print(ori[i-1::-1])
+                return ori[i-1::-1]
+                #break
+
+            if s == s[::-1]:
+                indx = len(ori) - len(s)
+                #print(ori[indx - 1::-1])
+                return len(ori[indx - 1::-1])
+                #break
+
+            i += 1
+
+if __name__ == "__main__":
+    s = input()
+
+    print(convertPalindrome(s))
     
