1b2fd7d88231919811fa887d964429e8c46684fd
@SuhasBRao@ <raos04567@gmail.com>
2022-02-15 20:32:58 +0530

    added a find_digits solution

---
+++ Hackerrank solutions/Problem solving/find_digits.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # Write your code here
+    nos = list(str(n))
+    nos = [int(x) for x in nos]
+    cnt = 0
+    
+    for no in nos:
+        if no!= 0 and n%no == 0:
+            cnt += 1
+    return cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
