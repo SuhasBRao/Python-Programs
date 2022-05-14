634709a3cf7a43ed92c8c5836813330072fb175d
@SuhasBRao@ <raos04567@gmail.com>
2022-05-14 18:36:08 +0530

    modified a bubble sort

--- Python files/Sorting Algorithms/Brute-force/Bubble_sort.py
+++ Python files/Sorting Algorithms/Brute-force/Bubble_sort.py
@@ -2,18 +2,32 @@
 This program implements bubble sort algorithm to sort
 an array of size n.
 '''
-from sys import stdin, stdout
-def getInts():
+from sys import stdin
+
+def getInputs():
     return list(map(int, stdin.readline().strip().split()))
 
-arr = getInts()
-
-for i in range(len(arr) - 1):
+def bubble_sort(alist):
+    # The function returns a sorted list by taking unsorted list as input
+    size = len(alist)
     
-    for j in range(len(arr) - 1 - i ):
-        if arr[j + 1] < arr[j]:
-            arr[j+1],arr[j] = arr[j],arr[j+1]
+    for i in range(size - 1):
+        
+        for j in range(size - 1 - i ):
+            
+            if alist[j + 1] < alist[j]:
+                alist[j+1],alist[j] = alist[j],alist[j+1]
     
+    return alist
+        
+# {
+# Driver Code starts
+if __name__ == "__main__":
+    # write your code here
+    
+    unsorted_list = getInputs()
+    sorted_list = bubble_sort(unsorted_list)
+    print(f'final sorted array : {sorted_list}')
 
+# } Driver Code ends
 
-print(f'final sorted array : {arr}')
